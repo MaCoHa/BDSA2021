1d35377be38acdec83afec456627f6f9a5eedc1c
MaCoHa <74664350+MaCoHa@users.noreply.github.com>
2021-09-05 16:44:54 +0200

    Exercise 7 error handling (#3)
    * Implemented error handling.

--- Hello.Tests/UnitTest1.cs
+++ Hello.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Hello;
+using System.IO;
 
 namespace Hello.Tests
 {
@@ -10,14 +11,22 @@ namespace Hello.Tests
         public void Is_Leap_Year()
         {
             var hello = new Program();
-            Assert.True(hello.IsLeapYear(200));
+            Assert.True(hello.IsLeapYear(2000));
         }
          [Fact]
         public void is_not_Leap_year()
         {
             var hello = new Program();
-            Assert.False(hello.IsLeapYear(230));
+            Assert.False(hello.IsLeapYear(2333));
         }
+          [Fact]
+        public void year_is_less_than_1582()
+        {
+          var hello = new Program();
+            Assert.False(hello.IsLeapYear(400));
+
+        }
+         
 
     }
 }
