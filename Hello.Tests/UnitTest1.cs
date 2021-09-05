54a5498f3b7ce684c696994cdf16abe76a904fda
MaCoHa <74664350+MaCoHa@users.noreply.github.com>
2021-09-05 15:11:21 +0200

    pull Exercise 5 - Leap year function og Exercise 6 - User input (#2)
    * igen da
    
    * Tested and implemented IsLeapYear.

--- Hello.Tests/UnitTest1.cs
+++ Hello.Tests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
 using Xunit;
+using Hello;
 
 namespace Hello.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Is_Leap_Year()
         {
-
+            var hello = new Program();
+            Assert.True(hello.IsLeapYear(200));
         }
+         [Fact]
+        public void is_not_Leap_year()
+        {
+            var hello = new Program();
+            Assert.False(hello.IsLeapYear(230));
+        }
+
     }
 }
