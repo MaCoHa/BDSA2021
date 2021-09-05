db20084dcdbd02168c69ba459ab34dc561be524a
coha <coha@itu.dk>
2021-09-05 16:42:18 +0200

    Implemented error handling.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -4,26 +4,39 @@ namespace Hello
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string valsting;
             int value;
             var hello = new Program();
             Console.Write("Enter Year: ");
             valsting = Console.ReadLine();
+            try{
             value = int.Parse(valsting);
+            
             
                         if(hello.IsLeapYear(value)){
                             Console.Write("yay " + value + " Is a Leap Year");
 
+                        }else if (value>1581) {
+
+                            Console.Write("Year " + value + " Is less than 1582");
                         }else{
+                            
                             Console.Write("nay " + value + " Is not a Leap Year");
 
                         }
+                        }catch(Exception ){
+                             Console.Write("not a year");
+                                                     
+                
+            }
         }
 
         public bool IsLeapYear(int year){
-            if ((year % 4) == 0){
+            if(year<1581){
+                return false;
+            }else if ((year % 4) == 0){
                 return true;
             }else{
                 return false;
